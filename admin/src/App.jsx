160f42380d17703be6f48bd6b855ff174c01b464
Aditya Kumar <adityakumar.fullstack@gmail.com>
2026-09-03 23:30:33 +0530

    Create sidebar and set up routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/admin/Dashboard';
+import AllAppointments from './pages/admin/AllAppointments';
+import AddDoctor from './pages/admin/AddDoctor';
+import DoctorList from './pages/admin/DoctorList';
 
 const App = () => {
   const { aToken } = useContext(AdminContext)
@@ -11,6 +17,16 @@ const App = () => {
     <main className="bg-[#F8F9FD]">
       <ToastContainer />
       <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorList />} />
+        </Routes>
+      </div>
     </main>
   ) : (
     <main>
---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { NavLink } from 'react-router-dom';
+import { assets } from '../assets/assets';
+
+const Sidebar = () => {
+    const { aToken } = useContext(AdminContext);
+    return (
+        <aside className="sidebar-wrapper h-screen bg-white border-r border-gray-200 p-2 min-w-[250px]">
+            {
+                aToken &&
+                <div className="sidebar">
+                    <div className="sidebar-inner">
+                        <ul className="sidebar-menu">
+                            <li className="sidebar-item">
+                                <NavLink to="/admin-dashboard" className={({ isActive }) => `sidebar-link flex items-center gap-2 py-2 px-5 md:px-8 md:py-3 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+                                    <span><img src={assets.home_icon} alt="" /></span>
+                                    Dashboard
+                                </NavLink>
+                            </li>
+                            <li className="sidebar-item">
+                                <NavLink to="/all-appointments" className={({ isActive }) => `sidebar-link flex items-center gap-2 py-2 px-5 md:px-8 md:py-3 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+                                    <span><img src={assets.appointment_icon} alt="" /></span>
+                                    Appointments
+                                </NavLink>
+                            </li>
+                            <li className="sidebar-item">
+                                <NavLink to="/add-doctor" className={({ isActive }) => `sidebar-link flex items-center gap-2 py-2 px-5 md:px-8 md:py-3 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+                                    <span><img src={assets.add_icon} alt="" /></span>
+                                    Add Doctor
+                                </NavLink>
+                            </li>
+                            <li className="sidebar-item">
+                                <NavLink to="/doctor-list" className={({ isActive }) => `sidebar-link flex items-center gap-2 py-2 px-5 md:px-8 md:py-3 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+                                    <span><img src={assets.people_icon} alt="" /></span>
+                                    Doctors
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            }
+        </aside >
+    )
+}
+
+export default Sidebar
